f6388c880b05a0d3979d8424ad552f1ef55e8169
Okke Harsta <oharsta@zilverline.com>
2018-11-27 09:22:41 +0100

    WIP

--- library/testing.py
+++ library/testing.py
@@ -4,16 +4,18 @@ import os, json
 import re, sys
 
 
-def firstProg(text):
+def first_prog(text):
     text1 = "Hello " + text
     return text1
 
 
 if __name__ == '__main__':
+    print("hello")
     fields = {
         "yourName": {"required": True, "type": "str"}
     }
     module = AnsibleModule(argument_spec=fields)
+    print(list(module.params.keys))
     yourName = os.path.expanduser(module.params['yourName'])
-    newName = firstProg(yourName)
+    newName = first_prog(yourName)
     module.exit_json(msg=newName)
